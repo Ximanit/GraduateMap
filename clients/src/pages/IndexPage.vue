741a430231ee2d987d78af1d4f1f635dc82ce8e8
Ximanit <122241859+Ximanit@users.noreply.github.com>
2023-09-22 23:17:44 +1000

    Создал проект, добавил карту начало положено

---
+++ clients/src/pages/IndexPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="center">
+    <div id="map" style="width: 300px; height: 300px"></div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  mounted() {
+    this.getmap();
+  },
+  methods: {
+    async getmap() {
+      DG.then(function () {
+        DG.map("map", {
+          center: [54.98, 82.89],
+          zoom: 13,
+        });
+      });
+    },
+  },
+};
+</script>
